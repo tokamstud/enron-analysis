166c7e0fc0475b9d8305eddc5beaa1d53a156334
tokams <t.gliniecki@hotmail.com>
2016-10-21 16:27:47 +0200

    python suks

--- count_empl.py
+++ count_empl.py
@@ -4,22 +4,17 @@ from mrjob.job import MRJob
 class MRCountSum(MRJob):
 
     def mapper(self, _, line):
-        line = line.strip()  # remove leading and trailing whitespace
-
-        if line.find("From:") == 0:
-
+		line = line.strip()  # remove leading and trailing whitespace
+		if line.find("From:") == 0:
 			login = line[line.find(" ")+1:line.find("@")]
+			if len(login) == 0:
+				login == "empty"
+			yield login, 1
+	def combiner(self, key, values):
+		yield key, sum(values)
 
-            if len(login) == 0:
-                login == "empty"
-            yield login, 1
-
-    def combiner(self, key, values):
-        yield key, sum(values)
-
-    def reducer(self, key, values):
-        yield key, sum(values)
-
+	def reducer(self, key, values):
+		yield key, sum(values)
 
 if __name__ == '__main__':
     MRCountSum.run()
